dded600c87ba8dd3fcfec1717050084c387373b0
Stefan Wouda <stefan.wouda@live.nl>
2020-05-01 23:28:01 +0200

    Update #1

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,7 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
 
+"use strict" ;
+
+// globale declaraties:
